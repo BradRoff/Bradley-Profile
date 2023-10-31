d97e0caee451bf5c28210b6c474e01145d6d4d1a
BradRoff <149277339+BradRoff@users.noreply.github.com>
2023-10-31 18:53:57 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* script.js */
+function openNav() {
+    document.querySelector(".sidenav").style.width = "220px";
+}
+
+function closeNav() {
+    document.querySelector(".sidenav").style.width = "0";
+}
