7f0dddd32bd120f2e511abc22ca47939c8191304
BradRoff <149277339+BradRoff@users.noreply.github.com>
2023-11-01 15:57:39 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /* script.js */
+
 function openNav() {
     document.querySelector(".sidenav").style.width = "220px";
 }
@@ -6,35 +7,71 @@ function openNav() {
 function closeNav() {
     document.querySelector(".sidenav").style.width = "0";
 }
-
+/*
+var MenuChoice ="";
 function SwitchButtons(buttonId) {
-    var hideBtn, showBtn;
+    var hideBtn;   showBtn;
     
     if (buttonId == 'moonEBtn') {
-      menuToggle = 'moonEBtn';
+        MenuChoice ="Norm"
       showBtn = 'sunBtn';
       hideBtn = 'moonEBtn';
+      
       document.body.style.backgroundColor = "#A9A9A9";
+      // Calling the jquery function with Javscript function
+     
     } else if  (buttonId == 'sunBtn'){
-      menuToggle = 'sunBtn';
+        MenuChoice ="Bright"
       showBtn = 'moonFBtn';
       hideBtn = 'sunBtn';
+      
       document.body.style.backgroundColor =  "#B3D9D9";
       
     }
     else if  (buttonId == 'moonFBtn'){
-      menuToggle = 'moonFBtn';
+        MenuChoice ="Dark"
       showBtn = 'moonEBtn'; 
       hideBtn = 'moonFBtn';
       document.body.style.backgroundColor = "#25383C";
 
     }
     
-    
+    $.jQueryChange(MenuChoice);
     //I don't have your menus, so this is commented out.  just uncomment for your usage
     // document.getElementById(menuToggle).toggle(); //step 1: toggle menu
     document.getElementById(hideBtn).style.display = 'none'; //step 2 :additional feature hide button
     document.getElementById(showBtn).style.display = ''; //step 3:additional feature show button
    
   }
- 
+*/
+    $(document).ready(function() {
+        $("#normal").click(function() {
+            // Code to run when the image is clicked
+            $(this).hide();
+            $("#bright").show();
+            document.body.style.backgroundColor = "#A9A9A9";
+            
+            var svg = $(".icon");
+            var path = svg.find("path");
+            path.attr("fill", "black");
+            document.body.style.color="black";
+        });
+        $("#bright").click(function() {
+            // Code to run when the image is clicked
+            $(this).hide();
+            $("#dark").show();
+            document.body.style.backgroundColor =  "#ADD8E6"
+          
+        });
+        $("#dark").click(function() {
+            // Code to run when the image is clicked
+            $(this).hide();
+            $("#normal").show();
+            document.body.style.backgroundColor = "#161618";
+            document.body.style.color="red";
+            var svg = $(".icon");
+            var path = svg.find("path");
+            path.attr("fill", "red");
+        });
+    });
+    
