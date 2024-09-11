32f511ac4f0fe4fb3b22206cf3e640e0f30d8ae9
BradRoff <149277339+BradRoff@users.noreply.github.com>
2024-09-11 19:45:14 -0400

    Add files via upload

--- send-email.php
+++ send-email.php
@@ -1,44 +1,56 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    /*
-     Collect and sanitize input data. 
-     */
-    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    // Your reCAPTCHA secret key
+    $recaptcha_secret = '6Lf3OT8qAAAAAKs8WuLcGt8B8RClQo2U-hZyBwIl';  
+    $recaptcha_response = filter_input(INPUT_POST, 'g-recaptcha-response', FILTER_SANITIZE_STRING);
 
-    // assign sanitized data to array postData for expected output
-    $postData = [
-        'name' => $name,
-        'email' => $email,
-        'message' => $message
-    ];
+    // Verify the reCAPTCHA response
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response");
+    $response_keys = json_decode($response, true);
 
-    /*
-    using and initializing cURL session (client URL) 
-    curl_init: encrypted url to send data
-    curl_setopt: set varios options
-    CURLOPT_RETURNTRANSFER: ensures outuput is returend as a string
-    CURLOPT_POST: ensures that the post method is being used
-    CURLOPT_POSTFIELDS: setts sanitized aray fields using http_build_query()
-    to encode the array into a URL encoded string.
-    */
-    // Initialize cURL
-
-    $ch = curl_init('3d6b734796a308f3c4f8a3666d08a29a');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
-
-    // Execute and closes to free resorces
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    // Check the response contains data, thus message has been sent. 
-    if ($response) {
-        echo 'Thank you for your message. It has been sent.';
+    // Check if reCAPTCHA was successfully verified
+    if (intval($response_keys["success"]) !== 1) {
+        echo 'Please complete the reCAPTCHA';
     } else {
-        echo 'Error sending message.';
+        /*
+         Collect and sanitize input data. 
+         */
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+
+        // Assign sanitized data to array postData for expected output
+        $postData = [
+            'name' => $name,
+            'email' => $email,
+            'message' => $message
+        ];
+
+        /*
+        Using and initializing cURL session (client URL) 
+        curl_init: encrypted url to send data
+        curl_setopt: set various options
+        CURLOPT_RETURNTRANSFER: ensures output is returned as a string
+        CURLOPT_POST: ensures that the POST method is being used
+        CURLOPT_POSTFIELDS: sets sanitized array fields using http_build_query()
+        to encode the array into a URL encoded string.
+        */
+        // Initialize cURL
+        $ch = curl_init('3d6b734796a308f3c4f8a3666d08a29a');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+
+        // Execute and close to free resources
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        // Check if the response contains data, thus the message has been sent. 
+        if ($response) {
+            echo 'Thank you for your message. It has been sent.';
+        } else {
+            echo 'Error sending message.';
+        }
     }
 } else {
     echo 'Invalid request.';
