2b4b656bc56dc0a61caaee76ac83da10a72878a7
BradRoff <149277339+BradRoff@users.noreply.github.com>
2024-09-11 20:13:14 -0400

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -1,63 +1,43 @@
 <?php
-if($_POST)
-{
-require('constant.php');
-    
-    $user_name      = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-    $user_email     = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $user_phone     = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
-    $content   = filter_var($_POST["content"], FILTER_SANITIZE_STRING);
-    
-    if(empty($user_name)) {
-		$empty[] = "<b>Name</b>";		
-	}
-	if(empty($user_email)) {
-		$empty[] = "<b>Email</b>";
-	}
-	if(empty($user_phone)) {
-		$empty[] = "<b>Phone Number</b>";
-	}	
-	if(empty($content)) {
-		$empty[] = "<b>Comments</b>";
-	}
-	
-	if(!empty($empty)) {
-		$output = json_encode(array('type'=>'error', 'text' => implode(", ",$empty) . ' Required!'));
-        die($output);
-	}
-	
-	if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
-	    $output = json_encode(array('type'=>'error', 'text' => '<b>'.$user_email.'</b> is an invalid Email, please correct it.'));
-		die($output);
-	}
-	
-	//reCAPTCHA validation
-	if (isset($_POST['g-recaptcha-response'])) {
-		
-		require('component/recaptcha/src/autoload.php');		
-		
-		$recaptcha = new \ReCaptcha\ReCaptcha(SECRET_KEY);
-
-		$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-
-		  if (!$resp->isSuccess()) {
-				$output = json_encode(array('type'=>'error', 'text' => '<b>Captcha</b> Validation Required!'));
-				die($output);				
-		  }	
-	}
-	
-	$toEmail = "member@testdomain.com";
-	$mailHeaders = "From: " . $user_name . "<" . $user_email . ">\r\n";
-        $mailBody = "User Name: " . $user_name . "\n";
-	$mailBody .= "User Email: " . $user_email . "\n";
-	$mailBody .= "Phone: " . $user_phone . "\n";
-	$mailBody .= "Content: " . $content . "\n";
-	if (mail($toEmail, "Contact Mail", $mailBody, $mailHeaders)) {
-	    $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_name .', thank you for the comments. We will get back to you shortly.'));
-	    die($output);
-	} else {
-	    $output = json_encode(array('type'=>'error', 'text' => 'Unable to send email, please contact'.SENDER_EMAIL));
-	    die($output);
-	}
+//Checking For reCAPTCHA
+$captcha;
+if (isset($_POST['g-recaptcha-response'])) {
+    $captcha = $_POST['g-recaptcha-response'];
+}
+// Checking For correct reCAPTCHA
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=SECRETKEY&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+if (!$captcha || $response.success == false) {
+    echo "Your CAPTCHA response was wrong.";
+    exit ;
+} else {
+    // Checking For Blank Fields..
+    if ($_POST["vname"] == "" || $_POST["vemail"] == "" || $_POST["msg"] == "") {
+        echo "Fill All Fields..";
+    } else {
+        // Check if the "Sender's Email" input field is filled out
+        $email = $_POST['vemail'];
+        // Sanitize E-mail Address
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        // Validate E-mail Address
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        if (!$email) {
+            echo "Invalid Sender's Email";
+        } else {
+            $to = 'johdoe@example.com';
+            $subject = 'Test';
+            $message = $_POST['msg'];
+            $headers = 'From:' . $email . "\r\n";
+            // Sender's Email
+            // Message lines should not exceed 70 characters (PHP rule), so wrap it
+            $message = wordwrap($message, 70, "\r\n");
+            // Send Mail By PHP Mail Function
+            if (mail($to, $subject, $message, $headers)) {
+                echo "Your mail has been sent successfully!";
+            } else {
+                echo "Failed to send email, try again.";
+                exit ;
+            }
+        }
+    }
 }
 ?>
